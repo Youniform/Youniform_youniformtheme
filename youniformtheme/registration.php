d2e437ee841104f15549edc0611ba7fd3d8b29d2
Youniform <youniform@yahoo.com>
2021-04-12 17:48:14 +0000

    init commit for youniformtheme

---
+++ youniformtheme/registration.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+# app/design/frontend/Youniform/youniformtheme; ?
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::THEME, 'frontend/Youniform/youniformtheme', __DIR__);
